e7412bf859bbae0c31f4331277a1738d5ec3d529
Elvis Serrão <elvis.serrao1@gmail.com>
2019-10-09 17:36:35 -0300

    Gem config

--- lib/txt_timesheet.rb
+++ lib/txt_timesheet.rb
@@ -1,17 +1,15 @@
 files = []
 hours = []
 test = []
-ihours = {}
-iminutes = {}
 minutes = []
 i_files = 0
 time_regex = /(?<hours>\d{2})\:(?<minutes>\d{2})/
 
-# Percorre todos os arquivos passados como argumento na linha de comando
+# Percorre todos os arquivos na linha de comando
 ARGV.each do|a|
   i = 0
   files[i_files] = a
-  file = "assets/#{a}"
+  file = "../assets/#{a}"
   content_file = File.open(file)
 
   # Percorre todas as linhas do arquivo de entrada para extrair os horários no formato hh:mm
@@ -34,19 +32,12 @@ end
 puts "REPORT:"
 i_files = 0
 
+#
 # Mostra o nome dos arquivos lidos e o total de horas de cada um
+#
 files.each do |fls|
   print "#{fls} "
-  print "#{ihours["#{i_files}"]}"
-  puts "#{iminutes["#{i_files}"] }"
+  print "#{hours}"
+  puts "#{minutes}"
   i_files+=1
 end
-
-
-# hora = []
-# i = 0
-# ihours.values_at("#{i}").each do |a|
-#   hora.push(a)
-#   puts hora[i]
-#   i+=1
-# end
---
+++ txt_timesheet.gemspec
@@ -0,0 +1,14 @@
+Gem::Specification.new do |s|
+### REQUIRED ###
+s.name        = 'txt_timesheet'
+s.version     = '0.0.1'
+s.summary     = "Timesheet"
+s.authors     = ["Elvis Serrão"]
+s.files       = ["lib/txt_timesheet.rb"]
+### WARNING ###
+s.homepage    = 'https://elvisserrao.github.io'
+s.license     = 'OML'
+### RECOMMENDED ###
+s.description = "An answer to a coding test" # nice to have
+s.email       = 'elvis.serrao1@gmail.com' # nice to have
+end
